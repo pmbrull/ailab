0e37bc7069026b61c563c1048c1507c5f89a05b4
Pere Miquel Brull <peremiquelbrull@gmail.com>
2024-05-09 18:17:10 +0200

    ownership

--- knowledge_graph/pipeline/pipeline/cypher/relationship.py
+++ knowledge_graph/pipeline/pipeline/cypher/relationship.py
@@ -14,12 +14,24 @@ from metadata.generated.schema.entity.data.table import Table
 from metadata.generated.schema.entity.services.databaseService import DatabaseService
 from metadata.generated.schema.entity.teams.team import Team
 from metadata.generated.schema.entity.teams.user import User
+from metadata.utils.constants import ENTITY_REFERENCE_CLASS_MAP
 
 from pipeline.cypher.base import CypherBase
 
 
 class CypherRel(CypherBase):
     """Create relationship queries"""
+
+    @staticmethod
+    def _get_ownership_rel(entity) -> str:
+        """Add ownership relationship"""
+        owner_type = ENTITY_REFERENCE_CLASS_MAP[entity.owner.type].__name__
+        return f"""
+        MATCH 
+          (o:{owner_type} {{fullyQualifiedName: '{entity.owner.fullyQualifiedName}'}}),
+          (d:{type(entity).__name__} {{fullyQualifiedName: '{entity.fullyQualifiedName.__root__}'}}) 
+        CREATE (o)-[:OWNS]->(d) 
+        """
 
     @singledispatchmethod
     def create_query(self, entity) -> list[str]:
@@ -71,6 +83,10 @@ class CypherRel(CypherBase):
     @create_query.register(Glossary)
     def _(self, entity) -> list[str]:
         """Nothing to do"""
+        rel_ = []
+        if hasattr(entity, "owner") and entity.owner:
+            rel_.append(self._get_ownership_rel(entity))
+        return rel_
 
     @create_query.register
     def _(self, entity: Tag) -> list[str]:
@@ -90,6 +106,9 @@ class CypherRel(CypherBase):
     def _(self, entity: GlossaryTerm) -> list[str]:
         """Create relationship with the classification"""
         rel_ = []
+        if hasattr(entity, "owner") and entity.owner:
+            rel_.append(self._get_ownership_rel(entity))
+
         if entity.parent:
             rel_.append(f"""
                 MATCH 
@@ -126,8 +145,61 @@ class CypherRel(CypherBase):
         return rel_
 
     @create_query.register(DatabaseService)
-    @create_query.register(Database)
-    @create_query.register(DatabaseSchema)
-    @create_query.register(Table)
     def _(self, entity) -> list[str]:
-        """Nothing to do"""
+        """Add owner"""
+        rel_ = []
+        if entity.owner:
+            rel_.append(self._get_ownership_rel(entity))
+
+        return rel_
+
+    @create_query.register
+    def _(self, entity: Database) -> list[str]:
+        """Add the service"""
+        rel_ = []
+        if entity.owner:
+            rel_.append(self._get_ownership_rel(entity))
+
+        if entity.service:
+            rel_.append(f"""
+            MATCH 
+              (d:Database {{fullyQualifiedName: '{entity.fullyQualifiedName.__root__}'}}),
+              (s:DatabaseService {{fullyQualifiedName: '{entity.service.fullyQualifiedName}'}}) 
+            CREATE (s)-[:CONTAINS]->(d) 
+            """)
+
+        return rel_
+
+    @create_query.register
+    def _(self, entity: DatabaseSchema) -> list[str]:
+        """Add the service"""
+        rel_ = []
+        if entity.owner:
+            rel_.append(self._get_ownership_rel(entity))
+
+        if entity.database:
+            rel_.append(f"""
+                MATCH 
+                  (s:DatabaseSchema {{fullyQualifiedName: '{entity.fullyQualifiedName.__root__}'}}),
+                  (d:Database {{fullyQualifiedName: '{entity.database.fullyQualifiedName}'}}) 
+                CREATE (d)-[:CONTAINS]->(s) 
+                """)
+
+        return rel_
+
+    @create_query.register
+    def _(self, entity: Table) -> list[str]:
+        """Add the service"""
+        rel_ = []
+        if entity.owner:
+            rel_.append(self._get_ownership_rel(entity))
+
+        if entity.database:
+            rel_.append(f"""
+                    MATCH 
+                      (t:Table {{fullyQualifiedName: '{entity.fullyQualifiedName.__root__}'}}),
+                      (s:DatabaseSchema {{fullyQualifiedName: '{entity.databaseSchema.fullyQualifiedName}'}}) 
+                    CREATE (s)-[:CONTAINS]->(t) 
+                    """)
+
+        return rel_
